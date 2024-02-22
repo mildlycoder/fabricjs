9e4743a0151db88390c0caf7bf8aa5dbd49532bb
Pranav Kumar <pranav.kumar@edufied.edu.vn>
2024-02-22 16:21:33 +0530

    listing all the variable

--- src/JsonReader.jsx
+++ src/JsonReader.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { fabric } from 'fabric';
 
 const JsonReader = () => {
   const fileInputRef = useRef(null);
   const canvasRef = useRef(null);
+  const [matchingVariables, setMatchingVariables] = useState([]);
 
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
@@ -45,20 +46,62 @@ const JsonReader = () => {
     });
 
     // Load objects from JSON onto the new canvas
-    canvas.loadFromJSON(parsedData, canvas.renderAll.bind(canvas));
+    canvas.loadFromJSON(parsedData, () => {
+      const matchingVars = findMatchingVariables(parsedData.objects);
+      setMatchingVariables(matchingVars);
+      canvas.renderAll();
+    });
   };
+
+  const findMatchingVariables = (objects) => {
+    console.log(objects)
+    const matchingVars = [];
+
+    objects.forEach((obj) => {
+      if (obj.id) {
+        matchingVars.push({ ...obj });
+      }
+    });
+    console.log(matchingVars)
+    return matchingVars;
+  };
+
+  useEffect(() => {
+    console.log('Matching Variables:', matchingVariables);
+    // You can perform any additional actions with the matchingVariables here
+  }, [matchingVariables]);
 
   return (
     <div className='m-10'>
-      <input type="file" ref={fileInputRef} onChange={handleFileChange} />
-      <button
-        onClick={() => {
-          // Trigger the file input
-          fileInputRef.current.click();
-        }}
-      >
-        Load JSON File
-      </button>
+     <div>
+        <input type="file" ref={fileInputRef} onChange={handleFileChange} />
+        <button
+            onClick={() => {
+            // Trigger the file input
+            fileInputRef.current.click();
+            }}
+        >
+            Load JSON File
+        </button>
+        <div className='mt-2 mb-2'>
+            <div className='mt-2 mb-2'>all varibles used:</div>
+            <div className='flex flex-col gap-2'>
+            {
+                matchingVariables.map((variable) => {
+                    return( 
+                        <div className='flex gap-2'>
+                            <div>{variable.id}</div>
+                            <input
+                            className='bg-gray-200 p-1'
+                            value={variable.text} 
+                            type="text" />
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
+     </div>
       {/* Canvas element for the JsonReader component */}
       <canvas className='' ref={canvasRef} />
     </div>
